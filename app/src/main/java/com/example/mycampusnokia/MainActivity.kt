ec160c4bf3c8c7f9e88cf48458268bc3a93a58a3
svidhaji <svidhaji@gmail.com>
2020-03-27 19:16:03 +0200

    add travis.yml

--- app/src/main/java/com/example/mycampusnokia/MainActivity.kt
+++ app/src/main/java/com/example/mycampusnokia/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        println("Hello world")
+        println("Hello, this is a test ")
     }
 }
