6c8e55cac19af98ec237c1c6def7091a7788ec0a
svidhaji <svidhaji@gmail.com>
2020-04-02 13:52:10 +0300

    Fix merge conflict and added assembleDebug

--- app/src/main/java/com/example/mycampusnokia/MainActivity.kt
+++ app/src/main/java/com/example/mycampusnokia/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mycampusnokia
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,7 +9,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
