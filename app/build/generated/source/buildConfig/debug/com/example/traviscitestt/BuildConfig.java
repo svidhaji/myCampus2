8bc46c9de8530e58a20e8b665f9d2b5251997da9
svidhaji <svidhaji@gmail.com>
2020-04-10 16:47:26 +0300

    test file added

--- app/build/generated/source/buildConfig/debug/com/example/traviscitestt/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/traviscitestt/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.traviscitestt;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.traviscitestt";
+  public static final String APPLICATION_ID = "com.example.mycampusnokia";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
